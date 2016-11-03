a483ec1a5ff5e4a114de858eaf3b1f7a7753d23e
Jaakko Salo <jaakko@tuxera.com>
2016-11-03 20:30:44 +0200

    Continue initial implementation.

--- PCF2123.cpp
+++ PCF2123.cpp
@@ -1,7 +1,7 @@
 /*
  * The MIT License (MIT)
  *
- * Copyright (c) 2016 Jaakko Salo
+ * Copyright (c) 2016 Jaakko Salo (jaakkos@gmail.com / jaakkos on Freenode)
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -22,12 +22,66 @@
  * SOFTWARE.
  */
 
-/* RW: write = 0, read = 1 */
+#include "PCF2123.h"
+
+#define SPI_MAX_SPEED 5000000
+
+#define CMD_READ 1
+#define CMD_WRITE 0
 #define CMD_BYTE(RW, ADDR) (0x10 | ((!!(RW)) << 7) | (ADDR))
+
+#define REG_CTRL1_ADDR      0x00
+#define REG_CTRL2_ADDR      0x01
+#define REG_TIME_DATE_ADDR  0x02
+
+bool
+PCF2123_CtrlReg::get(int bit)
+{
+  return this->ctrl & (1 << bit);
+}
+
+bool
+PCF2123_CtrlReg::set(int bit, bool value)
+{
+  bool old = this->get(bit);
+  if (value) this->ctrl |= (1 << bit);
+  else this->ctrl &= ~(1 << bit);
+  return old;
+}
+
+uint8_t PCF2123_CtrlReg::ctrl1_get() { return this->ctrl >> 8;   }
+uint8_t PCF2123_CtrlReg::ctrl2_get() { return this->ctrl & 0xFF; }
+
+void PCF2123_CtrlReg::ctrl1_set(uint8_t byte)
+{
+  this->ctrl = (uint16_t)this->ctrl2_get() + ((uint16_t)byte << 8);
+}
+
+void PCF2123_CtrlReg::ctrl2_set(uint8_t byte)
+{
+  this->ctrl = ((uint16_t)this->ctrl1_get() << 8) + (uint16_t)byte;
+}
+
+void PCF2123_CtrlReg::mask_alarms()
+{
+  this.set(MSF, true);
+  this.set(AF, true);
+  this.set(TF, true);
+}
 
 void
 PCF2123::rxt(uint8_t *buf, size_t sz)
 {
+  if (sz < 2)
+    return;
+
+  /*
+   * Zero out data if we're reading. This should help using
+   * the mode where MOSI and MISO are wired together at PCF2123.
+   */
+  if (buf[0] & 0x80)
+    memset(buf+1, 0, sz-1);
+
   SPI.beginTransaction(spi_cfg_);
   digitalWrite(ce_pin_, HIGH);
   SPI.transfer(buf, sz);
@@ -35,11 +89,130 @@ PCF2123::rxt(uint8_t *buf, size_t sz)
   SPI.endTransaction();
 }
 
+uint8_t
+PCF2123::bcd_decode(uint8_t bcd)
+{
+  return (bcd >> 4) * 10 + (bcd & 0x0F);
+}
+
+uint8_t
+PCF2123::bcd_encode(uint8_t dec)
+{
+  return ((dec / 10) << 4) || (dec % 10);
+}
+
 PCF2123::PCF2123(uint8_t cs_pin)
-  : cs_pin_(cs_pin), spi_cfg_(5000000, MSBFIRST, SPI_MODE0)
+  : cs_pin_(cs_pin), spi_cfg_(SPI_MAX_SPEED, MSBFIRST, SPI_MODE0)
 {
   pinMode(cs_pin_, OUTPUT);
   digitalWrite(cs_pin_, LOW);
+
+  /* Make sure the clock is in 24h mode */
+  PCF2123_CtrlRegs regs = this->ctrl_get();
+  regs.set(HOUR_MODE, 0);
+  this->ctrl_set(regs, true, false, true);
 }
 
+bool
+PCF2123::time_get(tmElements_t *now)
+{
+  uint8_t buf[1+7];
 
+  buf[0] = CMD_BYTE(CMD_READ, REG_TIME_DATE_ADDR);
+  rxt(buf, sizeof(buf));
+
+  now->Second = bcd_decode(buf[1] & ~0x80);
+  now->Minute = bcd_decode(buf[2] & ~0x80);
+  now->Hour   = bcd_decode(buf[3] & ~0xC0); /* 24h clock */
+  now->Day    = bcd_decode(buf[4] & ~0xC0);
+  now->Wday   = bcd_decode(buf[5] & ~0xF8);
+  now->Month  = bcd_decode(buf[6] & ~0xE0);
+  now->Year   = bcd_decode(buf[7]);
+
+  return buf[1] & 0x80;
+}
+
+void
+PCF2123::time_set(tmElements_t *new_time)
+{
+  uint8_t buf[1+7];
+
+  buf[0] = CMD_BYTE(CMD_WRITE, REG_TIME_DATE_ADDR);
+  buf[1] = bcd_encode(new_time->Second);
+  buf[2] = bcd_encode(new_time->Minute);
+  buf[3] = bcd_encode(new_time->Hour);
+  buf[4] = bcd_encode(new_time->Day);
+  buf[5] = bcd_encdoe(new_time->Wday);
+  buf[6] = bcd_encode(new_time->Month);
+  buf[7] = bcd_encode(new_time->Year);
+
+  rxt(buf, sizeof(buf));
+}
+
+void
+PCF2123::reset()
+{
+  uint8_t buf[2];
+  buf[0] = CMD_BYTE(CMD_WRITE, REG_CTRL1_ADDR);
+  buf[1] = 0x58;
+  rxt(buf, sizeof(buf));
+}
+
+PCF2123::clock_stop(bool stopped)
+{
+  PCF2123_CtrlRegs regs = ctrl_get();
+  regs.set(STOP, stopped);
+  this->ctrl_set(&regs, true,  /* Write ctrl1 */
+                        false, /* No ctrl2 */
+                        true   /* Mask alarms */);
+}
+
+PCF2123_CtrlRegs
+PCF2123::ctrl_get()
+{
+  uint8_t buf[1+2];
+  PCF2123_CtrlRegs regs;
+
+  buf[0] = CMD_BYTE(CMD_READ, REG_CTRL1_ADDR);
+  rxt(buf, sizeof(buf));
+
+  regs.ctrl1_set(buf[1]);
+  regs.ctrl2_set(buf[2]);
+  return regs;
+}
+
+void
+PCF2123::ctrl_set(const PCF2123_CtrlRegs *regs,
+                  bool set_ctrl1,
+                  bool set_ctrl2,
+                  bool mask_alarms)
+{
+  uint8_t buf[1+2];
+  int wrsz;
+
+  if (set_ctrl1 && set_ctrl2)
+  {
+    buf[0] = CMD_BYTE(CMD_WRITE, REG_CTRL1_ADDR);
+    buf[1] = regs->ctrl1_get();
+    buf[2] = regs->ctrl2_get();
+    wrsz = 3;
+  }
+
+  else if (set_ctrl1)
+  {
+    buf[0] = CMD_BYTE(CMD_WRITE, REG_CTRL1_ADDR);
+    buf[1] = regs->ctrl1_get();
+    wrsz = 2;
+  }
+
+  else if (set_ctrl2)
+  {
+    buf[0] = CMD_BYTE(CMD_WRITE, REG_CTRL2_ADDR);
+    buf[1] = regs->ctrl2_get();
+    wrsz = 2;
+  }
+
+  else return;
+
+  this->rxt(buf, wrsz);
+}
--- PCF2123.h
+++ PCF2123.h
@@ -1,7 +1,7 @@
 /*
  * The MIT License (MIT)
  *
- * Copyright (c) 2016 Jaakko Salo
+ * Copyright (c) 2016 Jaakko Salo (jaakkos@gmail.com / jaakkos on Freenode)
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -25,6 +25,81 @@
 #ifndef __PCF2123_H__
 #define __PCF2123_H__
 
+/* https://github.com/PaulStoffregen/Time */
+#include <Time.h>
+
+/**
+ * Structure for holding and manipulating PCF2123 control registers
+ * (1 and 2). Use PCF2123::ctrl_get() to get an instance.
+ */
+struct PCF2123_CtrlRegs
+{
+  /**
+   * PCF2123 registers (1 bit/register). Default values
+   * marked with asterisk.
+   *
+   * Enum values are bit positions into the registers.
+   */
+
+  enum Ctrl1Regs {  EXT_TEST  = 7+8,  /* 0*: normal mode, 1: external clock test mode */
+                    STOP      = 5+8,  /* 0*: RTC clock runs, 1: RTC clock stopped */
+                    SR        = 4+8,  /* 0*: no software reset, 1: initiate software reset */
+                    HOUR_MODE = 2+8,  /* 0*: 24h mode, 1: 12h mode. We only use 24h mode */
+                    CIE       = 1+8   /* 0*: no correction interrupt generated, 1: pulse
+                                         generated at every correction cycle */
+  };
+
+  enum Ctrl2Regs {  MI    = 7,  /* 0*: minute int. disabled, 1: minute int. enabled */
+                    SI    = 6,  /* 0*: second int. disabled, 1: second int. enabled */
+                    MSF   = 5,  /* 0*: no m/s int. generated, 1: int. has been generated */
+                    TI_TP = 4,  /* 0*: int. follows timer flags, 1: int. generates pulse */
+                    AF    = 3,  /* 0*: no alarm int. generated, 1: alarm triggered */
+                    TF    = 2,  /* 0*: no countdown int. generated, 1: countdown triggd */
+                    AIE   = 1,  /* 0*: don't generate alarm int., 1: generate alarm int. */
+                    TIE   = 0   /* 0*: don't generate countdown int., 1: generate countdown
+                                   int. */
+  };
+
+  uint16_t ctrl; /**< Control registers 1 (hi byte) and 2 (lo byte) */
+
+  /**
+   * Returns value of the given property.
+   *
+   * @param     bit     Register bit to return
+   *
+   * @return    Bit value
+   */
+  bool get(int bit);
+
+  /**
+   * Sets the value of the given property.
+   *
+   * @param     bit     Register bit to modify
+   * @param     value   New bit value
+   *
+   * @return    Old bit value
+   */
+  bool set(int bit, bool value);
+
+  /**
+   * Get/set control register bytes
+   */
+  uint8_t ctrl1_get();
+  uint8_t ctrl2_get();
+  void ctrl1_set(uint8_t byte);
+  void ctrl2_set(uint8_t byte);
+
+  /**
+   * Set all interrupt bits high. This will cause these bits
+   * to be ignored if the register is written, because the RTC
+   * will internally "AND" the current state with written bits.
+   */
+  void mask_alarms();
+};
+
+/**
+ * PCF2123 driver
+ */
 class PCF2123
 {
   private:
@@ -41,6 +116,24 @@ class PCF2123
      */
     void rxt(uint8_t rw, uint8_t addr, uint8_t *buf, size_t sz);
 
+    /**
+     * Parse a BCD-encoded decimal into decimal.
+     *
+     * @param   bcd     The encoded decimal
+     *
+     * @return  Decoded decimal
+     */
+    uint8_t bcd_decode(uint8_t bcd);
+
+    /**
+     * Encode a decimal into BCD
+     *
+     * @param   dec     Decimal to encode
+     *
+     * @return  Encoded value
+     */
+    uint8_t bcd_encode(uint8_t dec);
+
   public:
     /**
      * Construct a new instance of the driver.
@@ -51,13 +144,54 @@ class PCF2123
 
     /**
      * Get current time of the RTC.
+     *
+     * @param   now     Current time is written here
+     *
+     * @return  True if clock source integrity was
+     *          guaranteed
      */
-    clock_get();
+    bool time_get(tmElements_t *now);
 
     /**
      * Set current time of the RTC.
+     *
+     * @param   new_time      New time to set
      */
-    clock_set();
+    void time_set(tmElements_t *new_time);
+
+    /**
+     * Reset the RTC.
+     * NXP recommends doing this after powering on.
+     */
+    void reset();
+
+    /**
+     * Stop/resume the RTC.
+     *
+     * @param  stopped  Whether the clock should be stopped
+     *                  or running
+     */
+    void clock_stop(bool stopped);
+
+    /**
+     * Read control registers.
+     *
+     * @return  Current register state
+     */
+    PCF2123_CtrlRegs ctrl_get();
+
+    /**
+     * Write control register(s).
+     * Either ctrl1, ctrl2 or both can be written in the same
+     * transaction.
+     *
+     * @param   regs        Register buffer
+     * @param   set_ctrl1   Write ctrl1 register
+     * @param   set_ctrl2   Write ctrl2 register
+     * @param   mask_alarms Set alarm bits high to not affect alarm state
+     */
+    void ctrl_set(const PCF2123_CtrlRegs *regs, bool set_ctrl1, bool set_ctrl2,
+                  bool mask_alarms);
 };
 
 #endif
